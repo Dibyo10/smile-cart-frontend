bf1196537d2e87a9e83198410c585aa07135e710
Dibyo Chakraborty <dibyo.dc@gmail.com>
2026-02-04 22:47:19 +0530

    Used useState hook for carousel behaviour

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,24 +1,42 @@
+import { useState } from "react";
+
 import { Left, Right } from "neetoicons";
 import { Button } from "neetoui";
 
-const Carousel = ({ imageUrls, title }) => (
-  <div className="flex items-center">
-    <Button
-      className="shrink-0 focus-within:ring-0 hover:bg-transparent"
-      icon={Left}
-      style="text"
-    />
-    <img
-      alt={title}
-      className="max-w-56 h-56 max-h-56 w-56"
-      src={imageUrls[0]}
-    />
-    <Button
-      className="shrink-0 focus-within:ring-0 hover:bg-transparent"
-      icon={Right}
-      style="text"
-    />
-  </div>
-);
+const Carousel = ({ imageUrls, title }) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handleNext = () => {
+    const nextIndex = (currentIndex + 1) % imageUrls.length;
+    setCurrentIndex(nextIndex);
+  };
+
+  const handlePrev = () => {
+    const prevIndex = (currentIndex - 1 + imageUrls.length) % imageUrls.length;
+    setCurrentIndex(prevIndex);
+  };
+
+  return (
+    <div className="flex items-center">
+      <Button
+        className="shrink-0 focus-within:ring-0 hover:bg-transparent"
+        icon={Left}
+        style="text"
+        onClick={handlePrev}
+      />
+      <img
+        alt={title}
+        className="max-w-56 h-56 max-h-56 w-56"
+        src={imageUrls[currentIndex]}
+      />
+      <Button
+        className="shrink-0 focus-within:ring-0 hover:bg-transparent"
+        icon={Right}
+        style="text"
+        onClick={handleNext}
+      />
+    </div>
+  );
+};
 
 export default Carousel;
