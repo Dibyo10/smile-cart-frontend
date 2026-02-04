36f7e8f38545b6f31ea8220bda1d570f18c60a68
Dibyo Chakraborty <dibyo.dc@gmail.com>
2026-02-04 22:26:48 +0530

    Carousel Component and Product page enhancement via Neeto UI left and right for carousel behaviour

---
+++ src/Button.jsx
@@ -0,0 +1,7 @@
+const CustomButton = () => (
+  <button className="cursor-pointer rounded border-none bg-black px-1.5 py-2 text-white">
+    Click me
+  </button>
+);
+
+export default CustomButton;
